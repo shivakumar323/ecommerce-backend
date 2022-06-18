c02820fa4d75df35fb09762561637eb0963e5d84
shivakumar323 <shivanavodaya007@gmail.com>
2022-06-18 17:47:54 +0530

    changing routes to api/

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var logger = require('morgan');
 var cors = require('cors');
 const { httpCodes, httpColorCodes } = require("./src/constants/backendConfig");
 
-var indexRouter = require('./routes/api/v1/index');
-// var apiRouter = require('./routes/api');
+//var indexRouter = require('./routes/api/v1/index');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -45,8 +45,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
-app.use('/', indexRouter);
-// app.use('/api', apiRouter);
+//app.use('/', indexRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
